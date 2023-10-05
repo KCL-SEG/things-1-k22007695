f10832d70528058c6ffa3f3893bdcf788ae54307
k22007695 <k22007695@kcl.ac.uk>
2023-10-05 15:54:54 +0100

    User model validation

--- things/models.py
+++ things/models.py
@@ -1,9 +1,15 @@
+from django.core.validators import MaxValueValidator,MinValueValidator,RegexValidator
 from django.db import models
 
 # Create your models here.
 #Thing classetitiy with attribues bame description quantity
-class things(models):
-    name = models.CharField(max_length=100)
-    description=models.TextField()
-    quantity=models.IntegerField()
+class things():
+    name = models.CharField(max_length=30,
+                            unique=True,
+                            blank=False,
+                            validators=[RegexValidator(
+                                message='name must be unique not blank and at most 30 char')])
+    description=models.TextField(max_length=120)
+    quantity=models.IntegerField(validators=[MaxValueValidator(100),
+                                             MinValueValidator(1)])
 
