a0dd09d53f567e4c0ca1dcdd35416990be354970
k22007695 <k22007695@kcl.ac.uk>
2023-10-05 16:48:12 +0100

    All tests work

--- things/models.py
+++ things/models.py
@@ -3,13 +3,16 @@ from django.db import models
 
 # Create your models here.
 #Thing classetitiy with attribues bame description quantity
-class things():
+class Thing(models.Model):
     name = models.CharField(max_length=30,
                             unique=True,
                             blank=False,
                             validators=[RegexValidator(
                                 message='name must be unique not blank and at most 30 char')])
-    description=models.TextField(max_length=120)
+    description=models.CharField(max_length=120,
+                                 null=True,
+                                 blank=True,
+                                 validators=[RegexValidator(message='Description must be at most 120 characters')])
     quantity=models.IntegerField(validators=[MaxValueValidator(100),
-                                             MinValueValidator(1)])
+                                             MinValueValidator(0)])
 
