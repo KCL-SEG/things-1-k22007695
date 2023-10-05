2f7da1f11738674434061357758dedda747be8b8
k22007695 <k22007695@kcl.ac.uk>
2023-10-05 15:10:38 +0100

    thing model add

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#Thing classetitiy with attribues bame description quantity
+class things(models):
+    name = models.CharField(max_length=100)
+    description=models.TextField()
+    quantity=models.IntegerField()
+
